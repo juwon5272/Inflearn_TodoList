37a317c8e031d3b1d8620009501963669d522363
juwon5272 <jackson5272@naver.com>
2023-04-11 23:19:52 +0900

    feature : todo list 목록 추가하기
    input 태그 안에 todo 입력 후 엔터 누르면 아래에 추가됨

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const keyCodeCheck = function () {
-  const inputValue = document.querySelector("#todo-input").value;
-  console.log(inputValue);
+  if (window.event.keyCode === 13) {
+    const todoList = document.querySelector("#todo-list");
+    const inputValue = document.querySelector("#todo-input").value;
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+
+    newSpan.textContent = inputValue;
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+  }
 };
